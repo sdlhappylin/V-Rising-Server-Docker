1c888a191bba0e5bcce5fe43e43e61f5db2948ef
Albert <sdlhappylin@gmail.com>
2022-06-05 20:29:35 +0800

    Update and rename run.sh to docker-entrypoint.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod -R 777 /vrising/.wine/drive_c/VRisingServer
-chown -R vrising:vrising /vrising/.wine/drive_c/VRisingServer/server-data
-gosu vrising
-tini -- xvfb-run -a "wine ./VRisingServer.exe -persistentDataPath ./server-data
