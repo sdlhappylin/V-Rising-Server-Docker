464f948cee273403694d5d8da90ad8b10811f5c0
Albert <sdlhappylin@gmail.com>
2022-06-06 23:31:00 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-9001}
-cp /vrising/.wine/drive_c/VRisingServer/VRisingServer_Data/StreamingAssets/Settings /vrising/.wine/drive_c/VRisingServer/server-data
 groupadd -g ${GROUP_ID} vrising && \
     useradd -m -s /bin/false -u ${USER_ID} -g ${GROUP_ID} vrising && \
     chmod -R 777 /vrising/.wine/drive_c/VRisingServer && \
