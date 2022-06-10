044ae100abc16d5ca4c16148e7eae40614cd5cef
Albert <sdlhappylin@gmail.com>
2022-06-10 16:15:41 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,6 @@ USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-9001}
 groupadd -g ${GROUP_ID} vrising && \
     useradd -m -s /bin/false -u ${USER_ID} -g ${GROUP_ID} vrising && \
-    chmod -R 777 /vrising/.wine/drive_c/VRisingServer && \
-    chown -R vrising:vrising /vrising/.wine/drive_c/VRisingServer/server-data
+    chown -R vrising:vrising /vrising/.wine/drive_c/VRisingServer
 
 exec gosu vrising "$@"
